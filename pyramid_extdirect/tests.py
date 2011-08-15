4d74c2f130e5fc807863e39246fd1582cac1a55c
Igor Stroh <igor.stroh@rulim.de>
2011-08-15 13:16:28 +0200

    added more tests

--- pyramid_extdirect/tests.py
+++ pyramid_extdirect/tests.py
@@ -2,7 +2,7 @@ import unittest
 from pyramid import testing
 
 
-class TestRegisterPyramidExtDirect(unittest.TestCase):
+class TestPyramidExtDirect(unittest.TestCase):
 
     def setUp(self):
         from pyramid_extdirect import includeme
@@ -32,14 +32,41 @@ class TestRegisterPyramidExtDirect(unittest.TestCase):
         return self.config.registry.getUtility(IExtdirect)
 
     def test_register_class_method(self):
-        from pyramid_extdirect import tests
         dec = self._makeOne(action='MyAction')
         class MyClass(object):
             @dec
             def my_foo(self):
                 pass
-        my_cls = MyClass()
-        dec.register(self, 'my_foo', my_cls.my_foo)
+        dec.register(self, 'my_foo', MyClass.my_foo)
         self.assertEqual(dec._settings["original_name"], 'my_foo')
         self.assertEqual(dec._settings["action"], 'MyAction')
 
+    def test_api_dict(self):
+        dec = self._makeOne(action='MyAction')
+        class MyClass(object):
+            @dec
+            def my_foo(self):
+                pass
+        dec.register(self, 'my_foo', MyClass.my_foo)
+
+        dec2 = self._makeOne(action='OtherAction')
+        def bar(one, two): pass
+        decorated_bar = dec2(bar)
+        dec2.register(self, 'bar', bar)
+
+        dec3 = self._makeOne(action='UploadAction', accepts_files=True)
+        def upload(form): pass
+        decorated_upload = dec3(upload)
+        dec3.register(self, 'upload', upload)
+
+        util = self._get_util()
+        acts_dict = util.get_actions()
+
+        expected = [{'name': 'my_foo', 'len': 1}]
+        self.assertEqual(acts_dict['MyAction'], expected)
+
+        expected = [{'name': 'bar', 'len': 2}]
+        self.assertEqual(acts_dict['OtherAction'], expected)
+
+        expected = [{'name': 'upload', 'len': 1, 'formHandler': True}]
+        self.assertEqual(acts_dict['UploadAction'], expected)
