d9d12961fae57492c5761911dad7312341af9ba8
Igor Stroh <igor.stroh@rulim.de>
2011-08-15 17:50:52 +0200

    added more tests

--- pyramid_extdirect/tests.py
+++ pyramid_extdirect/tests.py
@@ -2,6 +2,18 @@ import unittest
 from pyramid import testing
 
 
+class Dummy(object):
+    pass
+
+
+class DummyAjaxRequest(testing.DummyRequest):
+
+    def __init__(self, params=None, environ=None, headers=None, path='/',
+            cookies=None, post=None, body='', **kw):
+        super(DummyAjaxRequest, self).__init__(params, environ, headers, path,
+                cookies, post, **kw)
+        self.body = body
+
 class TestPyramidExtDirect(unittest.TestCase):
 
     def setUp(self):
@@ -9,6 +21,13 @@ class TestPyramidExtDirect(unittest.TestCase):
         self.config = testing.setUp()
         includeme(self.config)
         self.config.end()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def _get_util(self):
+        from pyramid_extdirect import IExtdirect
+        return self.config.registry.getUtility(IExtdirect)
 
     def _makeOne(self, **options):
         from pyramid_extdirect import extdirect_method
@@ -26,10 +45,6 @@ class TestPyramidExtDirect(unittest.TestCase):
         decorated = dec(my_foo)
         dec.register(self, 'my_foo', my_foo)
         self.assertEqual(dec._settings["action"], 'SomeAction')
-
-    def _get_util(self):
-        from pyramid_extdirect import IExtdirect
-        return self.config.registry.getUtility(IExtdirect)
 
     def test_register_class_method(self):
         dec = self._makeOne(action='MyAction')
@@ -70,3 +85,49 @@ class TestPyramidExtDirect(unittest.TestCase):
 
         expected = [{'name': 'upload', 'len': 1, 'formHandler': True}]
         self.assertEqual(acts_dict['UploadAction'], expected)
+
+    def test_request_as_last_param(self):
+        dec = self._makeOne(action='FooAction', request_as_last_param=True)
+        def foo(request): pass
+        decorated = dec(foo)
+        dec.register(self, 'foo', foo)
+
+        util = self._get_util()
+        api_desc = util.get_actions()['FooAction'][0]
+        self.assertEqual(api_desc['len'], 0)
+
+    def test_simple_call(self):
+        dec = self._makeOne(action='SimpleAction')
+        def foo(param):
+            return param + ' was handled'
+        decorated = dec(foo)
+        dec.register(self, 'foo', foo)
+
+        util = self._get_util()
+        body = """{"action": "SimpleAction", "method": "foo", "data":["sample request"], "tid":0}"""
+        request = DummyAjaxRequest(body=body)
+        response, is_form_data = util.route(request)
+        self.failUnless('sample request was handled' in response)
+        self.failUnless(not is_form_data)
+
+    def test_file_upload(self):
+        dec = self._makeOne(action='SimpleUpload', accepts_files=True)
+        def do_upload(upload_data):
+            return {'success': True}
+        decorated = dec(do_upload)
+        dec.register(self, 'do_upload', do_upload)
+
+        util = self._get_util()
+        dummy_file = Dummy()
+        params = dict(
+            extAction='SimpleUpload',
+            extMethod='do_upload',
+            extTID='0',
+            extUpload='1',
+            extType='foo',
+        )
+        post=dict(uploadedFile=dummy_file)
+        request = testing.DummyRequest(params=params, post=post)
+        response, is_form_data = util.route(request)
+        self.failUnless(is_form_data)
+        self.failUnless('&quot;result&quot;: {&quot;success&quot;: true}' in response)
