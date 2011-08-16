f437dddb8f526cf59ce7dca7d7857ae7374606fc
Igor Stroh <igor.stroh@rulim.de>
2011-08-16 13:19:39 +0200

    updated version

--- pyramid_extdirect/__init__.py
+++ pyramid_extdirect/__init__.py
@@ -10,7 +10,7 @@ from zope.interface import implements
 from zope.interface import Interface
 import venusian
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 
 # form parameters sent by ExtDirect when using a form-submit
