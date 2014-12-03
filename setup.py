2e0a252bc2d9b3d4db46cea6dd533466ddb24c66
Igor Stroh <igor.stroh@rulim.de>
2014-12-03 09:36:50 +0100

    preparing release 0.5.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = ['pyramid', 'venusian']
 
 setup(name='pyramid_extdirect',
-    version='0.4.0',
+    version='0.5.0',
     description='ExtDirect Implementation for Pyramid',
     long_description=README + '\n\n' +  CHANGES,
     classifiers=[
